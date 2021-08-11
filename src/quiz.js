6554be988b628d5e9f452bf647a2c01993bf85b3
Jesinug <49886717+Jesinug@users.noreply.github.com>
2021-08-11 17:23:19 +0200

    Add files via upload

---
+++ src/quiz.js
@@ -0,0 +1,48 @@
+[
+    {
+        afirmation: { text:'All primitive data types are immutable', isCorrect: true }
+        /*answers: 
+        [
+            { ans1: 'true', isCorrect: true },
+            { ans2: 'false', isCorrect: false },
+
+        ],*/
+    };
+    { 
+        question: 'Lorgio has...?',
+        answers: 
+        [
+            { respuesta: '30', isCorrect: false},
+            { respuesta: '25', isCorrect: true },
+            { respuesta: '21', isCorrect: false },
+        ],
+    };
+    {
+        afirmation: { text:'Computers use 64bits to store a number', isCorrect: true }
+        /*answers: [
+            { ans1: 'true', isCorrect: true },
+            { ans2: 'false', isCorrect: false },
+        ]*/
+    };
+
+    {
+        afirmation: { text: 'this. points to the context whithin a function has being defined', isCorrect: false, correctAns: 'this. points to the context whithin a function has being called' }
+    };
+    
+    {
+        afirmation: { text: 'A JavaScript function always returns objects', isCorrect: false, correctAns: 'A JavaScript function always returns something, the what depends of the function' }
+    };
+    
+    {
+        afirmation: { text: 'One function can have more than one return statement', isCorrect: true }
+    };
+    
+    {};
+    
+    {};
+    
+    {};
+    
+    {};
+
+]
